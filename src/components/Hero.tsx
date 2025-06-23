9752e3e7962cc2bff4bcf25e8e01838a9d4ca7fc
Vlatko Jakimoski <jakimoski.vlatko@hotmail.com>
2025-06-23 20:06:53 +0200

    footer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,16 @@
 import BackgroundImg from "./BackgroundImg";
 import backgroundImage from "@/app/assets/Hero.webp";
+import TextMask from "./TextMask";
+
+const phrases = ["Железара", "Вејселоски"];
 
 export default function Hero() {
   return (
     <section className=" w-screen h-screen overflow-hidden relative">
       <BackgroundImg src={backgroundImage} hero={true} />
+      <div className="absolute hero-text top-1/2 left-1/2 transform text-9xl text-blue-700 font-bold uppercase -translate-x-1/2 -translate-y-1/2 w-full text-center">
+        <TextMask>{phrases}</TextMask>
+      </div>
     </section>
   );
 }
---
+++ src/components/TextMask.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { motion } from "framer-motion";
+
+export default function TextMask({ children }: { children: string[] }) {
+  return (
+    <div>
+      {children.map((phrase, index) => (
+        <div key={index} className="overflow-hidden">
+          <motion.h1
+            custom={index}
+            variants={{
+              initial: { y: "100%" },
+              enter: (i: number) => ({
+                y: "0",
+                transition: {
+                  duration: 0.75,
+                  ease: [0.33, 1, 0.68, 1],
+                  delay: i * 0.5,
+                },
+              }),
+            }}
+            initial="initial"
+            animate="enter"
+          >
+            {phrase}
+          </motion.h1>
+        </div>
+      ))}
+    </div>
+  );
+}
